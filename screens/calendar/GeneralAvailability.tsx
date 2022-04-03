16838c0c3c08bdf62db53ae8bf07292fb43d3e47
cjlaflamme1 <chadlaflamme@gmail.com>
2022-04-03 15:21:36 -0400

    basic accordion

--- screens/calendar/GeneralAvailability.tsx
+++ screens/calendar/GeneralAvailability.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons'; 
 import { View, StyleSheet, ScrollView } from 'react-native';
 import { Calendar } from 'react-native-calendars';
-import { Button, Text } from 'react-native-elements';
+import { Button, ListItem, Text } from 'react-native-elements';
 
 interface Props {};
 
 const GeneralAvailability: React.FC<Props> = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [expanded1, setExpanded1] = useState(false);
 
   return (
     <View>
@@ -14,7 +18,112 @@ const GeneralAvailability: React.FC<Props> = () => {
           style={[styles.calendar]}
         />
         <View style={[styles.buttonContainer]}>
-          <Text>Accordion component</Text>
+          <Button
+              title="Add New Date"
+              buttonStyle={[styles.buttonStyle]}
+            />
+        </View>
+        <View style={[styles.accordionContainer]}>
+          <ListItem.Accordion
+            style={[styles.accordion]}
+            isExpanded={expanded}
+            onPress={() => setExpanded(!expanded)}
+            content={
+              <>
+                <ListItem.Content style={[styles.accordion]}>
+                  <ListItem.Title>
+                    <Text>
+                      Every Thursday
+                    </Text>
+                  </ListItem.Title>
+                </ListItem.Content>
+              </>
+            }
+          >
+            <ListItem
+              style={[styles.accordionItem]}
+              containerStyle={[styles.accordionItem]}
+            >
+              <ListItem.Content>
+                <View style={[styles.accordionCard]}>
+                  <View>
+                    <Text>
+                      2pm-9pm
+                    </Text>
+                    <Text>
+                      Rumney
+                    </Text>
+                  </View>
+                  <View style={[styles.cardButtons]}>
+                    <FontAwesome
+                      style={[styles.cardIcon]}
+                      name="edit"
+                      size={24}
+                      color="black"
+                      onPress={() => console.log('clicky')}
+                    />
+                    <FontAwesome
+                      style={[styles.cardIcon]}
+                      name="trash-o"
+                      size={24}
+                      color="black"
+                      onPress={() => console.log('clicky')}
+                    />
+                  </View>
+                </View>
+              </ListItem.Content>
+            </ListItem>
+          </ListItem.Accordion>
+          <ListItem.Accordion
+            style={[styles.accordion]}
+            isExpanded={expanded1}
+            onPress={() => setExpanded1(!expanded1)}
+            content={
+              <>
+                <ListItem.Content style={[styles.accordion]}>
+                  <ListItem.Title>
+                    <Text>
+                      Every Other Tuesday
+                    </Text>
+                  </ListItem.Title>
+                </ListItem.Content>
+              </>
+            }
+          >
+            <ListItem
+              style={[styles.accordionItem]}
+              containerStyle={[styles.accordionItem]}
+            >
+              <ListItem.Content>
+                <View style={[styles.accordionCard]}>
+                  <View>
+                    <Text>
+                      2pm-9pm
+                    </Text>
+                    <Text>
+                      Rumney
+                    </Text>
+                  </View>
+                  <View style={[styles.cardButtons]}>
+                    <FontAwesome
+                      style={[styles.cardIcon]}
+                      name="edit"
+                      size={24}
+                      color="black"
+                      onPress={() => console.log('clicky')}
+                    />
+                    <FontAwesome
+                      style={[styles.cardIcon]}
+                      name="trash-o"
+                      size={24}
+                      color="black"
+                      onPress={() => console.log('clicky')}
+                    />
+                  </View>
+                </View>
+              </ListItem.Content>
+            </ListItem>
+          </ListItem.Accordion>
         </View>
       </ScrollView>
     </View>
@@ -30,9 +139,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  accordionContainer: {
+    marginTop: 20,
+  },
   buttonStyle: {
-    marginTop: 40,
+    marginTop: 20,
     minWidth: '50%',
+  },
+  accordion: {
+    width: '100%',
+  },
+  accordionItem: {
+    width: '100%',
+  },
+  accordionCard: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardButtons: {
+    flexDirection: 'row',
+  },
+  cardIcon: {
+    marginRight: 10,
   }
 })
 
